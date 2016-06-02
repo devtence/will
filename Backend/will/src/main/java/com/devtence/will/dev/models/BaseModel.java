1e4630a814c0c5e76960d1aac0c4f834f804011e
Plessmann <darthplessmann@gmail.com>
2016-06-02 15:53:07 -0400

    List from Datastore

--- Backend/will/src/main/java/com/devtence/will/dev/models/BaseModel.java
+++ Backend/will/src/main/java/com/devtence/will/dev/models/BaseModel.java
@@ -1,6 +1,13 @@
 package com.devtence.will.dev.models;
 
 
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.QueryResultIterator;
+import com.googlecode.objectify.cmd.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * base class for all the models of LP
  * Created by sorcerer on 7/18/15.
@@ -24,6 +31,45 @@ public abstract class BaseModel {
         DbObjectify.ofy().delete().entity(this).now();
     }
 
-    //TODO creat abstract class validateAndSave()
+    public static ListItem getList(String startAt, int limit, Class fromDb, String sortField, String sortDir) {
+        ListItem toReturn = new ListItem();
+
+        if (sortDir.equalsIgnoreCase("DESC")){
+            sortField = "-" + sortField;
+        }
+
+        Query query = DbObjectify.ofy().load().type(fromDb).order(sortField);
+
+        if (startAt != null) {
+            query = query.startAt(Cursor.fromWebSafeString(startAt));
+        }
+
+        int totalCount = query.count();
+
+        boolean cont = false;
+
+        // List for inserting the result items
+        ArrayList result = new ArrayList();
+        QueryResultIterator iterator = query.limit(limit).iterator();
+
+        while (iterator.hasNext()) {
+            result.add(iterator.next());
+            cont = true;
+        }
+
+        if (cont) {
+            Cursor cursor = iterator.getCursor();
+            toReturn.setItems(result);
+            toReturn.setCursor(cursor.toWebSafeString());
+        }
+
+        toReturn.setTotalCount(totalCount);
+
+        return toReturn;
+    }
+
+    public static List getAll(Class actual){
+        return DbObjectify.ofy().load().type(actual).list();
+    }
 
 }
