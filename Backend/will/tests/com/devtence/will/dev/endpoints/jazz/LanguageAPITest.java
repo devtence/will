f4be648f67e266167aa9c635fafd383d15e2d6b3
alfonso <alfbetan@gmail.com>
2016-06-15 16:26:47 -0400

    adding test classes for jazz

---
+++ Backend/will/tests/com/devtence/will/dev/endpoints/jazz/LanguageAPITest.java
@@ -0,0 +1,69 @@
+package com.devtence.will.dev.endpoints.jazz;
+
+import com.google.api.server.spi.auth.common.User;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cache.AsyncCacheFilter;
+import com.googlecode.objectify.util.Closeable;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sorcerer on 6/15/16.
+ */
+public class LanguageAPITest {
+
+    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+    private final static LanguageAPI actual = new LanguageAPI();
+    private final static User user = new User("ok", "email");
+    protected Closeable session;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        ObjectifyService.setFactory(new ObjectifyFactory());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.session = ObjectifyService.begin();
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        AsyncCacheFilter.complete();
+        this.session.close();
+        this.helper.tearDown();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testList() throws Exception {
+
+    }
+}
